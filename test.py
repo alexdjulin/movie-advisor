edd3ed87a71225b0feab9be6b4c0aaf6574cc10c
Alexandre D-Julin <53292656+adjulin@users.noreply.github.com>
2024-08-01 13:50:40 +0200

    delete test file

--- test.py
+++
@@ -1,9 +0,0 @@
-from ai_chitchat.terminal_colors import *
-
-print(f"{B_CYAN}This text is red{RESET}")
-print(f"{LIGHT_RED}This text is light red{RESET}")
-print(f"{BOLD_RED}This text is bold red{RESET}")
-print(f"{DIM_RED}This text is dim red{RESET}")
-print(f"{ITALIC}{B_CYAN}This text is red and italic{RESET}")
-print(f"{UNDERLINE}{B_CYAN}This text is red and underlined{RESET}")
-print(f"{B_CYAN}{CLEAR}This text is red and line cleared{RESET}")
