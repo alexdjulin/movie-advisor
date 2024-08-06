270f4b1f9af967790d4851c1085fd63cff7f4001
Alexandre D-Julin <53292656+adjulin@users.noreply.github.com>
2024-08-06 10:09:38 +0200

    renamed main into main_chatbot for clarity

--- main_chatbot.py
+++ main_chatbot.py
@@ -7,17 +7,17 @@ Author: @alexdjulin
 Date: 2024-07-25
 """
 
-# add chitchat module to path
+# add chatbot module to path
 import os
 import sys
-ai_chitchat_path = os.path.join(os.path.dirname(__file__), 'ai_chitchat')
-# sys.path.insert(0, ai_chitchat_path) # load ai_chitchat modules first in case of name duplicates
-sys.path.append(ai_chitchat_path) # load ai_chitchat modules last
+ai_chatbot_path = os.path.join(os.path.dirname(__file__), 'ai_chatbot')
+# sys.path.insert(0, ai_chatbot_path) # load ai_chatbot modules first in case of name duplicates
+sys.path.append(ai_chatbot_path) # load ai_chatbot modules last
 
 # parse command line arguments
 import argparse
 parser = argparse.ArgumentParser(description='Chat with an AI chatbot.')
-parser.add_argument('--config', '-c', type=str, default='ai_chitchat/config.yaml', help='Path to configuration file.')
+parser.add_argument('--config', '-c', type=str, default='ai_chatbot/config.yaml', help='Path to configuration file.')
 parser.add_argument('--input', '-i', type=str, help='Overrides input method to use: {text, voice, voice_k}.')
 parser.add_argument('--language', '-l', type=str, help='Overrides chat language (Example: en-US, fr-FR, de-DE). A matching voice should be defined in edgetts_voice, in the config file.')
 args = parser.parse_args()
