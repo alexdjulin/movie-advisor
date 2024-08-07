d169a1c3071dc3f0ef3e5f2612f089bce81a4e95
Alexandre D-Julin <53292656+adjulin@users.noreply.github.com>
2024-08-07 10:41:28 +0200

    edits to main text

--- main_text.py
+++ main_text.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Filename: main_text.py
+Description: Run the chatbot in text mode.
+Author: @alexdjulin
+Date: 2024-07-25
+"""
+
 import json
-import tools
+
 # env vars
 import dotenv
 dotenv.load_dotenv()
+
 # langchain
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-# openai
-embeddings = OpenAIEmbeddings()
-sep = 50 * "-"
+from langchain_openai import ChatOpenAI
+import tools
+
 # terminal colors
 CYAN = '\033[96m'
 MAGENTA = '\033[95m'
+GREY = '\033[90m'
 RESET = '\033[0m'
 
 
@@ -45,18 +53,20 @@ def main():
 
     # create langchain agent
     agent = create_tool_calling_agent(llm_gpt4, tools.agent_tools, prompt)
-    agent_executor = AgentExecutor(agent=agent, tools=tools.agent_tools, verbose=True)
+    agent_executor = AgentExecutor(agent=agent, tools=tools.agent_tools, verbose=False)
 
     messages = []
+
+    print(f"{GREY}# CHAT STARTED #{RESET}")
+    print(f"{GREY}Leave input empty to exit.{RESET}\n")
 
     # chat loop
     while True:
 
-        watch_lists = tools.get_watch_lists()
-        print(watch_lists)
-
         input_text = input(f"{CYAN}Alex: ")
         print(RESET)
+
+        # leave on empty input
         if not input_text:
             break
 
@@ -65,16 +75,17 @@ def main():
         result = agent_executor.invoke(
             {
                 "input": input_text,
-                "movie_history": [SystemMessage(content=watch_lists)],
                 "chat_history": messages,
             }
         )
 
         messages.append(AIMessage(content=result["output"]))
-        print(f"{MAGENTA}Marylin: ", result["output"], RESET)
-        print(sep)
+
+        print(f"{MAGENTA}Marylin: {result["output"]}{RESET}\n")
+
+    print(f"{GREY}# CHAT ENDED #{RESET}")
 
 
 if __name__ == "__main__":
     main()
-    print(RESET) # reset terminal colors
+    print(RESET)  # reset all terminal colors before leaving
